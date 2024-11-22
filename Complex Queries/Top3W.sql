f512de07983a7f405db4893e30d5bcfb33515855
khldnsser <kwnasser05@gmail.com>
2024-11-22 16:33:19 +0200

    Editing Top3W to only use Product Revenue

---
+++ Complex Queries/Top3W.sql
@@ -0,0 +1,23 @@
+WITH ProductRevenue AS (
+    SELECT 
+        w.WH_Name,
+        SUM(pt.Total_Revenue) AS product_revenue
+    FROM 
+        Warehouse w
+    JOIN 
+        Block b ON w.WH_Name = b.WH_Name
+    JOIN 
+        Product_Trans pt ON b.BK_ID = pt.Block_ID
+    WHERE 
+        pt.Transaction_Date >= NOW() - INTERVAL '1 year'
+    GROUP BY 
+        w.WH_Name
+)
+SELECT 
+    WH_Name,
+    product_revenue
+FROM 
+    ProductRevenue
+ORDER BY 
+    product_revenue DESC
+LIMIT 3;
